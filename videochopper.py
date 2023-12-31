d350474cd69f5caa813abcca6b72cb6f94873717
Andre Sadighi <andresadighi@protonmail.com>
2023-12-30 17:03:37 -0800

    Added Vertical Clip and delete clip option.

--- videochopper.py
+++ videochopper.py
@@ -1,7 +1,11 @@
 import os 
 import string
+import pathlib
 
-# For every file in directory, check if there's a txt file with the same name.
+#### 'True' or 'False' Case-Sensitive. ####
+make_vclip = True
+delete_clip = False
+####
 
 def getTimestamps(timestamp_filename):
     with open(timestamp_filename, 'r') as file_handle:
@@ -47,8 +51,13 @@ for video_filename in videoqueue:
 
         stop_time = timestamps[timestamp_counter+1]
 
-        output_name = f"{start_time.replace(':', '-')}_{stop_time.replace(':', '-')}_{video_filename}"
-        os.system(f"ffmpeg -ss {start_time} -i \"{video_filename}\" -to {stop_time} -c copy \"{output_name}\"")
-            
-        timestamp_counter += 1
+        output_name = f"clip_{start_time.replace(':', '-')}_{stop_time.replace(':', '-')}_{video_filename[:-4]}.mp4"
+        os.system(f"ffmpeg -ss {start_time} -to {stop_time} -i \"{video_filename}\" \"{output_name}\"")
+        
+        if make_vclip:
+            os.system(f"ffmpeg -i \"{output_name}\" -vf \"split[original][copy];[copy]scale=-1:ih*(16/9)*(16/9),crop=w=ih*9/16,gblur=sigma=20[blurred];[blurred][original]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2\" \"v{output_name}\"")
 
+        if delete_clip:
+            os.remove(pathlib.Path(f"{output_name}"))
+            
+        timestamp_counter += 2
