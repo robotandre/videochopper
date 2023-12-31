1a5af7ebe2774d747480671c7b8e67f5e399fef8
Andre Sadighi <andresadighi@protonmail.com>
2023-12-30 17:07:17 -0800

    Added experimental title option.

--- videochopper.py
+++ videochopper.py
@@ -2,10 +2,29 @@ import os
 import string
 import pathlib
 
-#### 'True' or 'False' Case-Sensitive. ####
-make_vclip = True
-delete_clip = False
+#####
+# Directions:
+# Make .txt file with same name as video file with timestamps formatted "(start)\n(stop)\n" 
+# Example (No spaces):
+### videofile.txt
+# 0:10
+# 0:20
+# 0:30
+# 0:40
+###
+# This will make a clip from 0:10 to 0:20, and another for 0:30 to 0:40. 
 ####
+
+###########################################
+#### 'True' or 'False' Case-Sensitive. ####
+make_vclip = False  # Vertical, phone resolution.
+delete_clip = False # Delete normal clip, keep Vclip.
+has_titles = False
+####
+def getTitles(title_filename):
+    with open(title_filename, 'r') as file_handle:
+        titlenames = file_handle.read().strip().split('\n')
+    return titlenames
 
 def getTimestamps(timestamp_filename):
     with open(timestamp_filename, 'r') as file_handle:
@@ -39,6 +58,11 @@ videoqueue = getVideoQueue()
 
 for video_filename in videoqueue:
     timestamps = getTimestamps(os.path.splitext(video_filename)[0] + ".txt")
+    
+    title_counter = 0
+    if has_titles: 
+        titles = getTitles(os.path.splitext(video_filename)[0] + ".title")
+        title_length = len(titles)
 
     timestamp_counter = 0
     timestamp_length = len(timestamps)
@@ -51,9 +75,17 @@ for video_filename in videoqueue:
 
         stop_time = timestamps[timestamp_counter+1]
 
+        output_metadata = ""
         output_name = f"clip_{start_time.replace(':', '-')}_{stop_time.replace(':', '-')}_{video_filename[:-4]}.mp4"
-        os.system(f"ffmpeg -ss {start_time} -to {stop_time} -i \"{video_filename}\" \"{output_name}\"")
+        os.system(f"ffmpeg -ss {start_time} -to {stop_time}{output_metadata} -i \"{video_filename}\" \"{output_name}\"")
+        ## Note that {output_metadata} starts as "" and any spaces will be appended if metadata exists.
+        ## Example: ' -metadata title="Movie Title"'
         
+        if has_titles:
+            output_metadata += f" -metadata title=\"{titles[title_counter]}\""
+            output_metadata += f" -metadata track=\"{title_counter+1}\""
+            os.system(f"ffmpeg -i \"{output_name}\" -codec copy{output_metadata} \"m{output_name}\"")
+
         if make_vclip:
             os.system(f"ffmpeg -i \"{output_name}\" -vf \"split[original][copy];[copy]scale=-1:ih*(16/9)*(16/9),crop=w=ih*9/16,gblur=sigma=20[blurred];[blurred][original]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2\" \"v{output_name}\"")
 
@@ -61,3 +93,4 @@ for video_filename in videoqueue:
             os.remove(pathlib.Path(f"{output_name}"))
             
         timestamp_counter += 2
+        title_counter += 1
